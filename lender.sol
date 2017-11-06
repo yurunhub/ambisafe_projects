d239a9f96ed4d08327b4d39d43e81915355af692
Yura Monchak <yurun@navohudonosor.interconnect.com.ua>
2017-11-06 17:36:36 +0200

    dd return values to public functions

--- lender.sol
+++ lender.sol
@@ -3,10 +3,10 @@ contract Lender {
     address public owner;
     uint public maxLendAmount;
     mapping(address => uint) public balanceOf;
-    
+
     event Borrowed(address who, uint amount);
     event Returned(address who, uint amount);
-    
+
     modifier onlyOwner() {
         if (msg.sender != owner) {
             return;
@@ -27,39 +27,43 @@ contract Lender {
         if(maxAmount > 100000000) {
             revert();
         }
-        
+
         maxLendAmount = maxAmount;
-    } 
-    
-    function changeMaxAmount (uint maxAmount)  onlyOwner() public {
+    }
+
+    function changeMaxAmount (uint maxAmount)  onlyOwner() public  returns(bool) {
         if( maxAmount > 100000000) {
             revert();
-        } 
+        }
         maxLendAmount = maxAmount;
+	return true;
     }
-    
-    function requestMoney(uint amount) onlyNotOwner()  public {
+
+    function requestMoney(uint amount) onlyNotOwner()  public returns(bool) {
         if(amount > maxLendAmount || balanceOf[msg.sender] + amount > maxLendAmount) {
             revert();
-        } 
+        }
 
-	if(amount == 0 ) {
+        if(amount == 0 ) {
             revert();
-	}
+        }
         balanceOf[msg.sender] += amount;
         Borrowed(msg.sender, amount);
+	return true;
     }
-    
-    function returnMoney(address who, uint amount) onlyOwner() public {
-        
+
+    function returnMoney(address who, uint amount) onlyOwner() public  returns(bool) {
+
         if(balanceOf[who] < amount ) {
             revert();
         }
         if(amount == 0 ) {
             revert();
         }
-       
+
         balanceOf[who] -= amount;
         Returned(who, amount);
+	return true;
     }
 }
+
